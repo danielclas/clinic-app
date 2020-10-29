3eeff08a08074bfe924bfc7922d22463f602f2f7
danielclas <danielclas@outlook.es>
2020-10-28 22:18:20 -0300

    Login funct. Added auth. service

--- clinica-app/src/app/app-routing.module.ts
+++ clinica-app/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,7 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: '', redirectTo: 'login', pathMatch: 'full'}
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
--- clinica-app/src/app/login/login.component.ts
+++ clinica-app/src/app/login/login.component.ts
@@ -1,15 +1,68 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  ngOnInit(): void {
+  constructor(
+      private formBuilder: FormBuilder,
+      private route: ActivatedRoute,
+      private router: Router,
+  ) {
+      // redirect to home if already logged in
+      // if (this.authenticationService.currentUserValue) {
+      //     this.router.navigate(['/']);
+      // }
   }
 
+  ngOnInit() {
+      this.loginForm = this.formBuilder.group({
+          email: ['', Validators.required, Validators.minLength(5)],
+          password: ['', Validators.required, Validators.minLength(5)]
+      });
+
+      // Get return url from route parameters or default to '/'
+      this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+
+  navigateToRegister(){
+    this.router.navigateByUrl('/register');
+  }
+
+  onSubmit() {
+
+      this.submitted = true;
+
+      if (this.loginForm.invalid) {
+          return;
+      }
+
+      // this.loading = true;
+
+      // this.authenticationService.login(this.f.username.value, this.f.password.value)
+      //     .pipe(first())
+      //     .subscribe(
+      //         data => {
+      //             this.router.navigate([this.returnUrl]);
+      //         },
+      //         error => {
+      //             this.alertService.error(error);
+      //             this.loading = false;
+      //         });
+  }
 }
